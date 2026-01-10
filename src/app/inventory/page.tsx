80eff8e19c139221738ddfe462c7d69d15d5fe49
Ahmad-Alfahad <ahmad222alfaha@gmail.com>
2026-01-11 02:37:20 +0300

    basic structure

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,10 +1,8 @@
-import { getItems } from "../service/api"
 export default  function InventoryPage(){
-   const items =  getItems();
+  
     return (
         <div>
             <h1>InventoryPage</h1>
-        
         </div>
     )
 }
